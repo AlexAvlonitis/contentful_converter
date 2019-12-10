0094986627be941ebaff4cf95a957ba39b151f75
Alex <avlonitis@msn.com>
2019-12-10 16:18:24 +0000

    Simplify tree cloner

--- lib/contentful_converter/tree_cloner.rb
+++ lib/contentful_converter/tree_cloner.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
-# Clones nokogiri fragment tree in a contentful specific tree structure
-# Keeps track of objects and their children with a hash_map,
-# value[0] index is always the corresponding rich_text node
-# value[1..] indexes are the children ids of the key:node
-# {
-#   nokogiri_obj_id: [rich_text_node, noko_child_obj_id2, noko_child_obj_id3],
-#   nokogiri_obj_id2: [rich_text_node]
-#   nokogiri_obj_id3: [rich_text_node]
-# }
-# TODO: Refactor
-
 require 'contentful_converter/node_builder'
 
 module ContentfulConverter
@@ -21,42 +10,33 @@ module ContentfulConverter
           return NodeBuilder.build(nokogiri_fragment).to_h
         end
 
-        pre_order_traversal_map(nokogiri_fragment)
-        construct_rich_text_tree
-
-        rich_text_root_node = @hash_map.first[1].first
-        rich_text_root_node.to_h
+        traverse_and_clone(nokogiri_fragment).to_h
       end
 
       private
 
-      def pre_order_traversal_map(nokogiri_fragment)
-        @hash_map = {}
-        stack = [nokogiri_fragment]
+      def traverse_and_clone(nokogiri_fragment)
+        rich_root_node = NodeBuilder.build(nokogiri_fragment)
 
-        while stack.any?
-          node = stack.pop
-          @hash_map[node.object_id] = [NodeBuilder.build(node)]
+        noko_stack = [nokogiri_fragment]
+        rich_stack = [rich_root_node]
 
-          next unless node.children.any?
+        while noko_stack.any?
+          noko_node = noko_stack.pop
+          rich_node = rich_stack.pop
 
-          node.children.each do |ch_node|
-            stack << ch_node
-            @hash_map[node.object_id] << ch_node.object_id
+          next unless noko_node.children.any?
+
+          noko_node.children.each do |child_node|
+            rich_child_node = NodeBuilder.build(child_node)
+
+            noko_stack << child_node
+            rich_stack << rich_child_node
+            rich_node.add_content(rich_child_node)
           end
         end
-      end
 
-      def construct_rich_text_tree
-        @hash_map.each do |_key, values|
-          rich_text_node = values.first
-          children_obj_ids = values[1..-1]
-          next if children_obj_ids.empty?
-
-          children_obj_ids.each do |obj_id|
-            rich_text_node.add_content(@hash_map[obj_id][0])
-          end
-        end
+        rich_root_node
       end
     end
   end
